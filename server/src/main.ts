bc50600b99f41cdd6b53303257296317be08f7fc
Void8500 <fz.omlan@gmail.com>
2026-02-23 18:35:55 +0600

    shifted backend from railway to render.com and db to neon.com

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Enable CORS so Frontend can talk to Backend
-  app.enableCors();
 
-  await app.listen(3000);
+  // Enable CORS so Frontend can talk to Backend
+  app.enableCors({
+    origin: ['https://lumexa-mu.vercel.app', 'http://localhost:3000'],
+    credentials: true,
+  });
+
+  // Render (and most cloud hosts) inject PORT dynamically
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
 bootstrap();
